0a5d7a1af866f94f4dea2b61456ca2c2922f0d69
nikos sideris <n.sideris@hotmail.com>
2014-10-12 12:26:58 +0000

    make it better4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 var fs=require('fs');
 var filename="index.html";
-var data=fs.readFileSynch(filename,'utf-8');
+var data=fs.readFileSync(filename,'utf-8');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
