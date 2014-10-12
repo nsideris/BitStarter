7b85dfb2c94b2cd58148108345c4e636987038f2
nikos sideris <n.sideris@hotmail.com>
2014-10-12 11:34:10 +0000

    ttt

---
+++ web.js
@@ -0,0 +1,14 @@
+77;10200;0cvar express = require('express')
+var app = express();
+var fs=require('fs');
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html'))
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
