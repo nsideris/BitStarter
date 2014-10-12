2e823d9ec205bd45a087576e5e5330e9474097c7
nikos sideris <n.sideris@hotmail.com>
2014-10-12 12:23:41 +0000

    make it better4

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(data)
+var buf=new Buffer (data);
+  response.send(buf.toString())
 })
 
 app.listen(app.get('port'), function() {
