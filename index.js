8fa3a0b14584d392b1bd09d93bd11f0ecc178670
nikos sideris <n.sideris@hotmail.com>
2014-10-12 11:48:57 +0000

    make it better2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'))
+  response.send("Hello world")
 })
 
 app.listen(app.get('port'), function() {
