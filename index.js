e7d727c0faca7e337d531aad3e13522a8a0413be
nikos sideris <n.sideris@hotmail.com>
2014-10-12 09:42:49 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World edited from emacs!')
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node App is running at localhost:" + app.get('port'))
+}) 
